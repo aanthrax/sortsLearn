e2110a980015f8fcad679064e0cba7dea168cee9
aaaxyettt <bedy.s.boshkoy@yandex.ru>
2023-02-18 16:29:38 +0300

    time limit added, but count sort doesn't work(

--- main.cpp
+++ main.cpp
@@ -6,78 +6,113 @@
 
 int main(){
         int step = 100;
-	std::cout << "SIZE" << "\t" << "bubble sort" << "\t" << "insertion sort" << "\t" << "selection sort" << "\t" << "quick sort" << "\t" << "merge sort" << "\t" << "count sort" << std::endl;
+	double time_bubble = 0;
+	int time_insertion = 0;
+	int time_selection = 0;
+	int time_quick = 0;
+	int time_merge = 0;
+	std::cout << "SIZE" << '\t' << "bubble sort" << '\t' << "insertion sort" << '\t' << "selection sort" << '\t' << "quick sort" << '\t' << "merge sort" << '\t' << std::endl;
         
-	for(int SIZE = 100; SIZE < 10000000; SIZE += step){
+	for(int SIZE = 100; SIZE <= 10000000; SIZE += step){
                 int* ar =  new int [SIZE];
-                randomFill(ar, SIZE, 0, 999);
-                std::cout << SIZE << "\t";
+                std::cout << SIZE << '\t';
 
                 //bubbleSort
-                std::chrono::time_point<std::chrono::high_resolution_clock> start;
-                std::chrono::duration<double> diff;
-                std::chrono::time_point<std::chrono::high_resolution_clock> end;
-                start = std::chrono::high_resolution_clock::now();
+		randomFill(ar, SIZE, 0, 999);
+		if(time_bubble < 60){
+                	std::chrono::time_point<std::chrono::high_resolution_clock> start;
+                	std::chrono::duration<double> diff;
+                	std::chrono::time_point<std::chrono::high_resolution_clock> end;
+                	start = std::chrono::high_resolution_clock::now();
 
-                bubbleSort(ar, SIZE);
+                	bubbleSort(ar, SIZE);
 
-                end = std::chrono::high_resolution_clock::now();
-                diff = end - start;
-                std::cout << diff.count() << "\t";
+                	end = std::chrono::high_resolution_clock::now();
+                	diff = end - start;
+                	std::cout << diff.count() << '\t';
+			time_bubble = diff.count();
+		}
+		else if(time_bubble >= 60){
+			std::cout << '\t';
+		}
 
                 //insertionSort
 		randomFill(ar, SIZE, 0, 999);
-                start = std::chrono::high_resolution_clock::now();
+		if(time_insertion < 60){
+                        std::chrono::time_point<std::chrono::high_resolution_clock> start;
+                        std::chrono::duration<double> diff;
+                        std::chrono::time_point<std::chrono::high_resolution_clock> end;
+                        start = std::chrono::high_resolution_clock::now();
 
+                        insertionSort(ar, SIZE);
 
-                insertionSort(ar, SIZE);
-
-                end = std::chrono::high_resolution_clock::now();
-                diff = end - start;
-                std::cout << diff.count() << "\t";
+                        end = std::chrono::high_resolution_clock::now();
+                        diff = end - start;
+                        std::cout << diff.count() << '\t';
+                        time_insertion = diff.count();
+                }
+                else if(time_insertion >= 60){
+			std::cout << '\t';
+                }
 
                 //selectionSort
 		randomFill(ar, SIZE, 0, 999);
-                start = std::chrono::high_resolution_clock::now();
+                if(time_selection < 60){
+                        std::chrono::time_point<std::chrono::high_resolution_clock> start;
+                        std::chrono::duration<double> diff;
+                        std::chrono::time_point<std::chrono::high_resolution_clock> end;
+                        start = std::chrono::high_resolution_clock::now();
 
-                selectionSort(ar, SIZE);
+                        selectionSort(ar, SIZE);
 
-                end = std::chrono::high_resolution_clock::now();
-                diff = end - start;
-                std::cout << diff.count() << "\t";
+                        end = std::chrono::high_resolution_clock::now();
+                        diff = end - start;
+                        std::cout << diff.count() << '\t';
+                        time_selection = diff.count();
+                }
+                else if(time_selection >= 60){
+                        std::cout << '\t';
+                }
 
 
                 //quickSort
 		randomFill(ar, SIZE, 0, 999);
-                start = std::chrono::high_resolution_clock::now();
+                if(time_quick < 60){
+                        std::chrono::time_point<std::chrono::high_resolution_clock> start;
+                        std::chrono::duration<double> diff;
+                        std::chrono::time_point<std::chrono::high_resolution_clock> end;
+                        start = std::chrono::high_resolution_clock::now();
 
-                quickSort(ar, SIZE);
+                        quickSort(ar, SIZE);
 
-                end = std::chrono::high_resolution_clock::now();
-                diff = end - start;
-                std::cout << diff.count() << "\t";
+                        end = std::chrono::high_resolution_clock::now();
+                        diff = end - start;
+                        std::cout << diff.count() << '\t';
+                        time_quick = diff.count();
+                }
+                else if(time_quick >= 60){
+                        std::cout << '\t';
+                }
 		
 
                 //mergeSort
 		randomFill(ar, SIZE, 0, 999);
-                start = std::chrono::high_resolution_clock::now();
+                if(time_merge < 60){
+                        std::chrono::time_point<std::chrono::high_resolution_clock> start;
+                        std::chrono::duration<double> diff;
+                        std::chrono::time_point<std::chrono::high_resolution_clock> end;
+                        start = std::chrono::high_resolution_clock::now();
 
-                mergeSort(ar, SIZE);
+                        mergeSort(ar, SIZE);
 
-                end = std::chrono::high_resolution_clock::now();
-                diff = end - start;
-                std::cout << diff.count() << "\t";
-
-                //countSort
-//                start = std::chrono::high_resolution_clock::now();
-
-//              int* smallAr[1000];
-//                countSort(ar, smallAr, SIZE);
-
-//                end = std::chrono::high_resolution_clock::now();
-//                diff = end - start;
-//                std::cout << diff.count() << "\t";
-
+                        end = std::chrono::high_resolution_clock::now();
+                        diff = end - start;
+                        std::cout << diff.count() << '\t';
+                        time_merge = diff.count();
+                }
+                else if(time_merge >= 60){
+                        std::cout << '\t';
+                }
 
 
                 std::cout<<std::endl;
